f5d34064b7f3fdeba6b159e97a0cbc8fc82f7855
Sir Alter <Sir Alter>
2016-11-26 14:22:29 +0300

    Initial folder structure created

---
+++ WebAPI/MyStudyProject.Data.Entities/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStudyProject.Data.Entities
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}

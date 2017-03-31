1bb5f794517fa33ad82bfc3e79eed077e6998bdd
MAIN\vbibikov <vbibikov@scnsoft.com>
2017-03-31 14:11:16 +0300

    Updated configuration

--- WebAPI/MyStudyProject.Data.DataAccess/Context/SqlApplicationDbContextFactory.cs
+++ WebAPI/MyStudyProject.Data.DataAccess/Context/SqlApplicationDbContextFactory.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
@@ -8,19 +8,23 @@ namespace MyStudyProject.Data.DataAccess.Context
 {
     public class SqlApplicationDbContextFactory : IDbContextFactory<SqlApplicationDbContext>
     {
+        private string ParentProject = @"src\MyStudyProject";
         private IConfigurationRoot configuration;
 
         public SqlApplicationDbContext Create(DbContextFactoryOptions options)
         {
+            var path = Path.Combine(Directory.GetParent(options.ContentRootPath).Parent.FullName, ParentProject);
             var builder = new ConfigurationBuilder()
-                .SetBasePath(options.ContentRootPath)
+                .SetBasePath(path)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{options.EnvironmentName}.json", optional: true);
-
+ 
             configuration = builder.Build();
 
+            Console.WriteLine(configuration.GetSection("AppSettings:ConnectionString").Value);
+            Console.WriteLine("###");
             var optionsBuilder = new DbContextOptionsBuilder<SqlApplicationDbContext>();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("AppSettings"), m => { m.EnableRetryOnFailure(); });
+            optionsBuilder.UseSqlServer(configuration.GetSection("AppSettings:ConnectionString").Value, m => { m.EnableRetryOnFailure(); });
 
             return new SqlApplicationDbContext(optionsBuilder.Options);
         }
